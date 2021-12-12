3becd5256399074f57ca518217812cd2c81b693a
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-12 13:33:38 -0300

    Create charts.py

---
+++ charts.py
@@ -0,0 +1,36 @@
+import plotly.express as px
+
+def scatter_chart(df):
+    fig = px.scatter(
+    df, 
+    x = 'happiness_score', 
+    y = 'country',
+    color = 'highlight',
+    height=4000,
+    hover_data = ['country', 'year', 'happiness_score'],
+    color_discrete_map={'none': 'white'}
+    )
+
+    fig.update_yaxes(
+        tickvals = df.country.unique(),              # make a line for each country
+        
+    )            
+
+    fig.update_xaxes(
+        tickwidth = 1,
+        range = (1, 8.2)
+    )
+
+    fig.update_traces(
+        marker = {
+            'size': 12,
+            'opacity':0.5,
+            'line':{'width':1, 'color': 'DarkSlateGrey'}
+        }
+        
+    )
+
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)'       # make the background transparent
+        # 'paper_bgcolor': 'rgba(0, 0, 0, 0)'
+    })
