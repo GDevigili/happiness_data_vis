caada751efcf49e6504c1892bb014837878b6c9a
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-12 14:05:55 -0300

    Add the chart to the interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,12 @@ import matplotlib.pyplot as plt
 import plotly
 import plotly.express as px
 
+# project files
+from charts import *
+
+# load files
+df = pd.read_pickle('data/df_happiness.pkl')
+
 # ---------------------------------------- #
 #              PAGE SETTINGS               #
 # ---------------------------------------- #
@@ -23,4 +29,10 @@ st.set_page_config(
     page_title = title
 )
 
+# ---------------------------------------- #
+#              PAGE ELEMENTS               #
+# ---------------------------------------- #
 
+st.title(title)
+
+st.plotly_chart(scatter_chart(df))
