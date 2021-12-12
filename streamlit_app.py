1aed010e6b5c5ef2962599d1a295f06e15301cea
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-12 12:14:22 -0300

    Add page settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,14 @@ import matplotlib.pyplot as plt
 import plotly
 import plotly.express as px
 
+# ---------------------------------------- #
+#              PAGE SETTINGS               #
+# ---------------------------------------- #
+
+title = 'World Happiness Report Data Vis'
+
+st.set_page_config(
+    page_title = title
+)
+
+
