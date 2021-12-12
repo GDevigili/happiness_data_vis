04a421339acc5a473760e7ad64eb32682a1bde76
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-12 12:05:15 -0300

    Add the requirements/imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+# ---------------------------------------- #
+#                 IMPORTS                  #
+# ---------------------------------------- #
+
+# interface
 import streamlit as st
+
+# data manipulation
+import pandas as pd
+
+# data vis
+import matplotlib.pyplot as plt
+import plotly
+import plotly.express as px
+
