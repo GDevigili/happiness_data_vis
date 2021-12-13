a9dc3846dc7ac5967119910447b8d94002f47788
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-13 15:33:31 -0300

    Edit labels

--- source/line.py
+++ source/line.py
@@ -2,6 +2,8 @@ import plotly.express as px
 from source.utils import *
 import streamlit as st
 
+gray = '#b7b7b7'
+
 def render_line(df, st_col):
     fig = px.line(
         # data
@@ -12,8 +14,38 @@ def render_line(df, st_col):
         #color 
         color = 'country',
 
+        # title and labels
+        title = 'Happiness Index Score by Country <br> <sup>Happiness over time</sup>',
+        labels = {
+            'happiness_score': 'Happiness Index Score',
+            'country': '',
+            'year':  ''
+        },
+
         # markers
         markers = True
+    )
+
+    fig.update_yaxes(
+        gridcolor = gray
+    )
+
+    fig.update_layout(
+        # background    
+        {
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)'
+        },
+
+        # fonts
+        font_family = 'Courier New',
+        title_font_family = 'Arial',
+
+        # legend
+        legend = dict(
+            orientation = 'v'
+        )
+
+
     )
 
     return st_col.plotly_chart(fig)
