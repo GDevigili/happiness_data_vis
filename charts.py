2d9a858a7625340d0510941fad670c354aa26ef5
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-12-12 22:38:29 -0300

    Separate the plots into subplots

--- charts.py
+++ charts.py
@@ -1,33 +1,37 @@
 import plotly.express as px
 from utils import *
 
-def scatter_chart(df, ascending = False):
+def scatter_chart(df, marker_color, ascending = False):
 
     fig = px.scatter(
-    df.sort_values(['happiness_score'], ascending = ascending), 
-    x = 'happiness_score', 
-    y = 'country',
-    color = 'highlight',
-    height = 200 + len(df.country.unique()) * 20,
-    hover_data = ['country', 'year', 'happiness_score'],
-    color_discrete_map={'none': 'white'}
+        df.sort_values(['happiness_score'], ascending = ascending), 
+        x =         'happiness_score', 
+        y =         'country',
+        color =     'highlight',
+        height =    200 + len(df.country.unique()) * 20,
+        hover_data = ['country', 'year', 'happiness_score'],
+        labels = {
+                    'x': 'Happiness Index Score',
+                    'y': None
+        }
     )
 
     fig.update_yaxes(
-        tickvals = df.country.unique(),              # make a line for each country
-        range=[-.5,len(df.country.unique())+.5]
+        tickvals =  df.country.unique(),              # make a line for each country
+        range=      [-.5,len(df.country.unique())+.5]
     )            
 
     fig.update_xaxes(
         tickwidth = 1,
-        range = (1, 8.2)
+        range =     (2, 8.2)
     )
 
     fig.update_traces(
         marker = {
-            'size': 12,
-            'opacity':0.5,
-            'line':{'width':1, 'color': 'DarkSlateGrey'}
+            'size':     12,
+            'opacity':  0.9,
+            'line':     {'width':1, 'color': 'DarkSlateGrey'},
+            'color':    marker_color
         }
         
     )
--- utils.py
+++ utils.py
@@ -1,11 +1,37 @@
 import streamlit as st
 import pandas as pd
 from charts import *
+import plotly.subplots 
 
-def df_mean(df, value_col, group_col):
+def get_df_mean(df, value_col, group_col):
     for col in df[group_col].unique():
         return df[df[group_col] == col].groupby(group_col)[value_col].mean()
 
 
 def render_scatter(df, sort = False):
-    return st.plotly_chart(scatter_chart(df, ascending = sort))
+    colors = {
+        '2020': '#3177CD',
+        '2021': '#051B33',
+        'Other years': '#E7E7E7',
+        'Mean': '#FFAD01'
+    }
+
+    df_others = df[~df['year'].isin([2020, 2021])]
+    fig_others = scatter_chart(df_others, colors['Other years'], sort)
+
+    df2020 = df[df['year'] == 2020]
+    fig2020 = scatter_chart(df2020, colors['2020'], sort)
+
+    df2021 = df[df['year'] == 2021]
+    fig2021 = scatter_chart(df2021, colors['2021'], sort)
+
+    # df_mean = get_df_mean(df, 'happiness_score', 'country')
+    # scatter_chart(df_mean, colors['Mean'], sort)
+
+    fig = plotly.subplots.make_subplots()
+    fig.add_trace(fig_others)
+    fig.add_trace(fig2020)
+    fig.add_trace(fig2021)
+    # fig.add_trace(df_mean)
+
+    return fig
